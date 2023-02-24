fd07e2953757790852532f92a395e37142cebbbe
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2023-02-24 15:15:48 +0100

    integration bootsrtapv5

--- events/views.py
+++ events/views.py
@@ -8,7 +8,7 @@ def HomePage(request):
     return HttpResponse('<h1>Hello from home page</h1>')
 
 def ListEvent(request):
-    list =Event.objects.all()
+    list =Event.objects.filter(state=True)
     return render(request,'events/EventList.html' , {'events' : list})
 
 
@@ -18,3 +18,6 @@ class ListEventView(ListView):
     model =Event
     template_name ="events/EventList.html"
     context_object_name ="events"
+    def get_queryset(self):
+        return Event.objects.filter(state=True)
+    
