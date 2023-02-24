d4246df96209f6286eb0cbddaf464d4c2013cff0
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2023-02-24 14:14:09 +0100

    ListView

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-     path('eventslist/', ListEvent ,name="list_events")
+     path('eventslist/', ListEvent ,name="list_events"),
+     path('eventslistView/' , ListEventView.as_view(),name='list_events_view') 
 ]
--- events/views.py
+++ events/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event
+from django.views.generic import ListView
 # Create your views here.
-
+#Views funtion based
 def HomePage(request):
     return HttpResponse('<h1>Hello from home page</h1>')
 
 def ListEvent(request):
     list =Event.objects.all()
     return render(request,'events/EventList.html' , {'events' : list})
+
+
+#Views Class Based
+
+class ListEventView(ListView):
+    model =Event
+    template_name ="events/EventList.html"
+    context_object_name ="events"
