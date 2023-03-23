1ce5f9049477892d09fc2e04bfef14a1e8ce882f
Oumeima-IbnElfekih <oumeimaibnelg@gmail.com>
2023-03-23 10:17:12 +0100

    add actions/filters/user backOffice changement

--- events/admin.py
+++ events/admin.py
@@ -1,9 +1,64 @@
-from django.contrib import admin
+from django.contrib import admin ,messages
 from .models import *
+from datetime import datetime
 # Register your models here.
+class ParticipationInline(admin.StackedInline):
+    model =Participation
+    extra=1
+    readonly_fields=('date_participation',)
+    can_delete=True
 
+
+class ParticipantFilter(admin.SimpleListFilter):
+    title='NBR Participant'
+    parameter_name ='nbe_participant'
+    def lookups(self,request,model_admin):
+        return (('0',("No Participants")),('more',("More participants")))
+    def queryset(self,request,queryset):
+        if self.value() =='0':
+            return queryset.filter(nbe_participant__exact=0)
+        if self.value() =='more':
+            return queryset.filter(nbe_participant__gt=0)
+class DateFilter(admin.SimpleListFilter):
+    title ='Event Date'
+    parameter_name ='evt_date'
+    def lookups(self,request,model_admin):
+        return (('Past events',("Past events")),
+                ('Upcomming Event',("Upcomming Event")),
+                ('Today Event',("Today Event")))
+    def queryset(self, request,queryset):
+        if self.value() =='Past events':
+            return queryset.filter(evt_date__lt=datetime.today())
+        if self.value() =='Upcomming Event':
+            return queryset.filter(evt_date__gt=datetime.today())
+        if self.value() =='Today Event':
+            return queryset.filter(evt_date__exact=datetime.today())
+
+def accept_events(model_admin,request,queryset):
+        rows_updated=queryset.update(state=True)
+        if rows_updated ==1:
+            msg="1 event"
+        else:
+            msg=f"{rows_updated} events"
+        messages.success(request,message= "%s successfully accepted " %msg)
+accept_events.short_description='Accept'
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    def set_to_Refuse(self, request, queryset):
+        rows_NoValid = queryset.filter(state=False)
+        if rows_NoValid.count() > 0:
+            messages.error(
+                request, f"{rows_NoValid.count()} events are already marked as Not Accepted")
+        else:
+            rows_updated = queryset.update(state=False)
+            if rows_updated == 1:
+                message = "1 event was"
+            else:
+                message = f"{rows_updated} events were"
+            self.message_user(request, level='success',
+                              message="%s successfully marked as Not Accepted" % message)
+
+    set_to_Refuse.short_description = "Refuse"
     def Participant_number(self,obj):
         val=obj.participations.count()
         return val
@@ -14,6 +69,8 @@ class EventAdmin(admin.ModelAdmin):
     ordering=('-title','-created_at','evt_date',)
     readonly_fields=('updated_at','created_at')
     autocomplete_fields=['organizer']
+    actions=[accept_events,'set_to_Refuse']
+    list_filter=('state','category',ParticipantFilter,DateFilter)
     fieldsets =(
         
         ('Event State' , {
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,47 @@
 from django.contrib import admin
-from .models import Person
+from .models import *
 # Register your models here.
 
 class PersonAdmin(admin.ModelAdmin):
-    search_fields=['first_name','last_name']
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'is_staff'
+                    )
+    list_filter = (
+        'is_staff',
+        'is_active',
+    )
 
-admin.site.register(Person,PersonAdmin)
+    search_fields = ['last_name', 'first_name']
+
+    readonly_fields = ('date_joined',)
+
+    fieldsets = (
+        (
+            None,
+            {"fields": ("cin", "username", "password")}
+        ),
+        (
+            "Personal info",
+            {
+                "fields": (("first_name", "last_name"), "email")}
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(Person, PersonAdmin)
